9be689a716280be03e1494e97bee15557816d347
Nurgiz <akkuzhin.n@gmail.com>
2023-10-07 12:28:58 +0400

    создание структуры таблиц

--- database/migrations/2023_10_07_065209_create_blog_posts_table.php
+++ database/migrations/2023_10_07_065209_create_blog_posts_table.php
@@ -15,7 +15,26 @@ class CreateBlogPostsTable extends Migration
     {
         Schema::create('blog_posts', function (Blueprint $table) {
             $table->increments('id');
+
+            $table->integer('category_id')->unsigned(); // id категории
+            $table->integer('user_id')->unsigned(); //id автора статьи
+
+            $table->string('slug')->unique();
+            $table->string('title');
+
+            $table->text('excerpt')->nullable(); // Выдержка статей
+            $table->text('content_raw'); // Контент markdown
+            $table->text('content_html'); // Автосохранение статьи в html (readOnly)
+
+            $table->boolean('is_published')->default(false); // опубликовано ли статья
+            $table->timestamp('published_at')->nullable(); //Когда статья была опубликована
+
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users'); //связь Users, подключаемся с id таблицы User
+            $table->foreign('category_id')->references('id')->on('blog_categories'); // подключаем id c blog_categories
+            $table->index('is_published');
         });
     }
 
